84423b41c11732f96b7fcd0205e7e71b485a2e05
Rithik M <rithik7899@gmail.com>
2024-07-26 23:57:13 +0530

    .

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,6 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return <div className="w-full h-full flex justify-center bg-signin/up-bg ">
-    <SignIn/>
+    <SignIn redirectUrl='https://tweetx-e310.onrender.com'/>
     </div>
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,6 +2,6 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return <div className="w-full h-full flex justify-center bg-signin/up-bg ">
-    <SignUp/>
+    <SignUp redirectUrl='/https://tweetx-e310.onrender.com/'/>
     </div>
 }
